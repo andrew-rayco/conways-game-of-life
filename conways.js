0fdbe627fdd71aad4f72b0fe24966f4e5c7f162d
andrew <andy@andycondon.com>
2017-04-28 17:19:37 +1200

    build a populated board

--- conways.js
+++ conways.js
@@ -1,6 +1,5 @@
 module.exports = {
-  createBoard,
-  // isAlive
+  createBoard
 }
 
 function createBoard(size) {
@@ -8,63 +7,19 @@ function createBoard(size) {
   for (var i = 0; i < size; i++) {
     board.push([])
     for (var j = 0; j < size; j++) {
-      board[i].push(0)
+      board[i].push(randomNumber())
     }
   }
-  // console.log(board)
   return board
 }
 
-// createBoard(10)
-// console.log(createBoard(10));
-console.log(populateBoard(createBoard(10)));
-
-function populateBoard(board) {
-  return board.map(function(row) {
-    return row.map(function(cell) {
-      var randomNumber = Math.random()
-      if (randomNumber < 0.45) {
-        return 1
-      } else {
-        return 0
-      }
-    })
-  })
+function randomNumber() {
+  var num = (Math.random() < 0.35) ? 1 : 0
+  return num
 }
 
-// function isAlive(cell) {
-//   if (cell == 1) {
-//     return true
-//   } else {
-//     return false
-//   }
-// }
-//
-// function checkAlive() {
-//   board.map(function(row) {
-//     row.map(function(cell) {
-//       if (isAlive(cell)) {
-//         cell.push(1)
-//       } else {
-//         cell.push(0)
-//       }
-//     })
-//   })
-// }
-
-function countAliveNeighbours(board, cell) {
-  for (var i = 0; i < array.length; i++) {
-    array[i]
-  }
-  console.log(board[0][3], board[1][3])
-
+function isOutOfBounds(row, column) {
+  
 }
-countAliveNeighbours(populateBoard(createBoard(10)))
 
-function aliveOrDead(liveNeighbours) {
-  if (liveNeighbours < 2 || liveNeighbours >= 4) {
-    return false
-  } else {
-    return true
-  }
-}
+console.log(createBoard(3));
--- test/test.js
+++ test/test.js
@@ -14,18 +14,3 @@ test('each row of the returned board is a different array', function(t) {
   t.notEqual(board[0], board[1])
   t.end()
 })
-
-// test('isAlive correctly returns dead status', function(t) {
-//   var cell = [0]
-//   var expected = false
-//
-//   t.equal(conways.isAlive(cell), expected, 'this cell is dead')
-//   t.end()
-// })
-//
-// test('isAlive correctly returns alive status', function(t) {
-//   var cell = [1]
-//
-//   t.equal(conways.isAlive(cell), true, 'this cell is alive')
-//   t.end()
-// })
