579405b4fe4110766360900c57964f10e3840707
Andrew <andy@andycondon.com>
2017-04-29 20:25:54 +1200

    make cells live or die correctly

--- conways.js
+++ conways.js
@@ -4,10 +4,13 @@ module.exports = {
 
 function runGame(size, population) {
   var board = createBoard(size, population)
+  console.log(board)
   processBoard(board)
-  getNeighbours(0, 0, board)
+  console.log('------------')
+  console.log(board)
+
 }
-runGame(6, 0.6)
+runGame(5, 0.6)
 
 // creates and randomly populates board
 function createBoard(size, population) {
@@ -28,37 +31,44 @@ function randomNumber(population) {
   return num
 }
 
-// return neighbours relative to cell being checked
-function getNeighbours(row, column, board) {
-  for (var i = row - 1; i < row + 2; i++) {
-    for (var j = column - 1; j < column + 2; j++) {
-      if ((i !== 0) || (j !== 0)) {
-        if (!outOfBounds(i, j, board)) {
-          console.log(i, j)
-          console.log('-----------');
-            // return board[i, j]
-        }
-      }
+function processBoard(board) {
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board.length; j++) {
+      var aliveNeighbourCount = 0
+      aliveNeighbourCount = getNeighbours(i, j, board)
+      board[i][j] = aliveOrDead(board[i][j], aliveNeighbourCount)
     }
   }
 }
 
-// getNeighbours(0, 0, board)
+function aliveOrDead(cell, aliveNeighbours) {
+  if (aliveNeighbours <= 1 || aliveNeighbours >= 4) {
+    return 0
+  } else if (cell == 0 && aliveNeighbours == 3){
+    return 1
+  } else {
+    return 1
+  }
+}
 
+// return neighbours relative to cell being checked
+function getNeighbours(row, column, board) {
+  var aliveNeighbourCount = 0;
+  for (var i = row - 1; i < row + 2; i++) {
+    for (var j = column - 1; j < column + 2; j++) {
+      if ((i !== row) || (j !== column)) {
+        if (!outOfBounds(i, j, board)) {
+          if (board[i][j] === 1) {
+            aliveNeighbourCount++
+          }
+        }
+      }
+    }
+  }
+  return aliveNeighbourCount
+}
+
+// check if any cells are out of bounds
 function outOfBounds(row, column, board) {
   return row < 0 || column < 0 || row >= board.length || column >= board.length
-}
-
-
-function processBoard(board) {
-  board.map(function(row) {
-    row.map(function(cell) {
-      getNeighbours(row, cell, board)
-    })
-  })
-}
-
-
-function isOutOfBounds(row, column) {
-
 }
