a6ea080e36a0a0e5fa3415dd056578d25546233c
Andrew <andy@andycondon.com>
2017-04-29 17:49:54 +1200

    add ability to define population in createBoard call

--- conways.js
+++ conways.js
@@ -2,24 +2,24 @@ module.exports = {
   createBoard
 }
 
-function createBoard(size) {
+function createBoard(size, population) {
   var board = [];
   for (var i = 0; i < size; i++) {
     board.push([])
     for (var j = 0; j < size; j++) {
-      board[i].push(randomNumber())
+      board[i].push(randomNumber(population))
     }
   }
   return board
 }
 
-function randomNumber() {
-  var num = (Math.random() < 0.35) ? 1 : 0
+function randomNumber(population) {
+  var num = (Math.random() < (population || 0.35)) ? 1 : 0
   return num
 }
 
 function isOutOfBounds(row, column) {
-  
+
 }
 
-console.log(createBoard(3));
+console.log(createBoard(6, 0.6));
